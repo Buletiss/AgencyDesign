7beed0cfa0eb07b5d1634e6c7b3b317d05d51c1b
ZeroBulletiss <vinicius1997feitosa@gmail.com>
2022-08-09 20:56:16 -0300

    infor image fix

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -5,110 +5,131 @@ import Portfolio3 from "../../assets/portfolio_3.png";
 import Portfolio4 from "../../assets/portfolio_4.png";
 
 export const Portfolio = () => {
-  return (
-    <Box>
-      <Flex flexDirection="column">
-        <Text
-          as="h1"
-          fontFamily="Poppins"
-          fontSize="20px"
-          fontWeight="500"
-          lineHeight="30px"
-          color="#20B15A"
-          mt="3rem"
-          ml="3rem"
-        >
-          OUR PORTFOLIO
-        </Text>
-        <Text
-          as="h2"
-          fontFamily="Poppins"
-          fontSize="30px"
-          fontWeight="600"
-          lineHeight="30px"
-          mt="3rem"
-          ml="3rem"
-          maxW="500px"
-        >
-          We provide the Perfect Solution to your business growth
-        </Text>
-        <Flex justify="center" p="30px" alignItems="center">
-          <SimpleGrid columns={2} spacing={10} alignItems="center">
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              gap="20px"
-              w="560px"
-            >
-              <Image src={Portfolio1} maxW="500px" w="100%" />
-              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
-                Digital Marketing Agency Website
-              </Text>
-              <Text
-                fontFamily="Poppins"
-                fontWeight="400"
-                lineHeight="21px"
-                fontSize="15px"
-              >
-                This is a website for a client who want to achieve their goals
-                and meet their users needs while also increasing their reach.
-                Accross all platforms. This is a website rebrand
-              </Text>
-            </Flex>
+    return (
+        <Box>
+            <Flex flexDirection="column">
+                <Text
+                    as="h1"
+                    fontFamily="Poppins"
+                    fontSize="20px"
+                    fontWeight="500"
+                    lineHeight="30px"
+                    color="#20B15A"
+                    mt="3rem"
+                    ml="3rem"
+                >
+                    OUR PORTFOLIO
+                </Text>
+                <Text
+                    as="h2"
+                    fontFamily="Poppins"
+                    fontSize="30px"
+                    fontWeight="600"
+                    lineHeight="30px"
+                    mt="3rem"
+                    ml="3rem"
+                    marginBottom="3rem"
+                    maxW="500px"
+                >
+                    We provide the Perfect Solution to your business growth
+                </Text>
+                <Flex justify="center" p="30px" alignItems="center">
+                    <SimpleGrid columns={2} spacing={10} alignItems="center">
+                        <Flex
+                            flexDirection="column"
+                            justifyContent="center"
+                            gap="20px"
+                            w="560px"
+                        >
+                            <Image src={Portfolio1} maxW="500px" w="100%" />
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                lineHeight="34px"
+                            >
+                                Digital Marketing Agency Website
+                            </Text>
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="400"
+                                lineHeight="21px"
+                                fontSize="15px"
+                            >
+                                This is a website for a client who want to
+                                achieve their goals and meet their users needs
+                                while also increasing their reach. Accross all
+                                platforms. This is a website rebrand
+                            </Text>
+                        </Flex>
 
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              gap="20px"
-              w="560px"
-            >
-              <Image src={Portfolio2} maxW="500px" w="100%" />
-              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
-                Digital Marketing Agency Website
-              </Text>
-              <Text>
-                This is a website for a client who want to achieve their goals
-                and meet their users needs while also increasing their reach.
-                Accross all platforms. This is a website rebrand
-              </Text>
-            </Flex>
+                        <Flex
+                            flexDirection="column"
+                            justifyContent="center"
+                            gap="20px"
+                            w="560px"
+                        >
+                            <Image src={Portfolio2} maxW="500px" w="100%" />
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                lineHeight="34px"
+                            >
+                                Digital Marketing Agency Website
+                            </Text>
+                            <Text>
+                                This is a website for a client who want to
+                                achieve their goals and meet their users needs
+                                while also increasing their reach. Accross all
+                                platforms. This is a website rebrand
+                            </Text>
+                        </Flex>
 
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              gap="20px"
-              w="560px"
-            >
-              <Image src={Portfolio3} maxW="500px" w="100%" />
-              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
-                Digital Marketing Agency Website
-              </Text>
-              <Text>
-                This is a website for a client who want to achieve their goals
-                and meet their users needs while also increasing their reach.
-                Accross all platforms. This is a website rebrand
-              </Text>
-            </Flex>
+                        <Flex
+                            flexDirection="column"
+                            justifyContent="center"
+                            gap="20px"
+                            w="560px"
+                        >
+                            <Image src={Portfolio3} maxW="500px" w="100%" />
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                lineHeight="34px"
+                            >
+                                Digital Marketing Agency Website
+                            </Text>
+                            <Text>
+                                This is a website for a client who want to
+                                achieve their goals and meet their users needs
+                                while also increasing their reach. Accross all
+                                platforms. This is a website rebrand
+                            </Text>
+                        </Flex>
 
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              gap="20px"
-              w="560px"
-            >
-              <Image src={Portfolio4} maxW="500px" w="100%" />
-              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
-                Digital Marketing Agency Website
-              </Text>
-              <Text>
-                This is a website for a client who want to achieve their goals
-                and meet their users needs while also increasing their reach.
-                Accross all platforms. This is a website rebrand
-              </Text>
+                        <Flex
+                            flexDirection="column"
+                            justifyContent="center"
+                            gap="20px"
+                            w="560px"
+                        >
+                            <Image src={Portfolio4} maxW="500px" w="100%" />
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                lineHeight="34px"
+                            >
+                                Digital Marketing Agency Website
+                            </Text>
+                            <Text>
+                                This is a website for a client who want to
+                                achieve their goals and meet their users needs
+                                while also increasing their reach. Accross all
+                                platforms. This is a website rebrand
+                            </Text>
+                        </Flex>
+                    </SimpleGrid>
+                </Flex>
             </Flex>
-          </SimpleGrid>
-        </Flex>
-      </Flex>
-    </Box>
-  );
+        </Box>
+    );
 };
--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,92 +1,105 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
-import Avatar from "../../assets/Avatar.png";
 
 export const Testimonials = () => {
-  const testimonial = [
-    {
-      note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
-      title: "Emily Stones",
-      occupation: "CEO, Marketing Guru",
-      image: { Avatar },
-    },
-    {
-      note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
-      title: "Stones Emily",
-      occupation: "Estagiaria de ti",
-      image: { Avatar },
-    },
-    {
-      note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
-      title: "Emilia Stones",
-      occupation: "Advocate",
-      image: { Avatar },
-    },
-  ];
+    const testimonial = [
+        {
+            note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
+            title: "Emily Stones",
+            occupation: "CEO, Marketing Guru",
+            image: require("../../assets/Avatar.png"),
+        },
+        {
+            note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
+            title: "Stones Emily",
+            occupation: "Estagiaria de ti",
+            image: require("../../assets/Avatar.png"),
+        },
+        {
+            note: "Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service",
+            title: "Emilia Stones",
+            occupation: "Advocate",
+            image: require("../../assets/Avatar.png"),
+        },
+    ];
 
-  return (
-    <Flex
-      h="
-    740px
-    "
-      w="100%"
-      gap="40px"
-      flexDirection="column"
-      justifyContent="center"
-    >
-      <Text
-        as="h1"
-        fontFamily="Poppins"
-        fontSize="20px"
-        fontWeight="500"
-        lineHeight="30px"
-        color="#20B15A"
-        mt="3rem"
-        ml="3rem"
-      >
-        Testimonials
-      </Text>
-      <Text
-        as="h2"
-        fontFamily="Poppins"
-        fontSize="30px"
-        fontWeight="600"
-        lineHeight="30px"
-        mt="3rem"
-        ml="3rem"
-        maxW="500px"
-      >
-        See what our customer say about us
-      </Text>
-
-      <Flex justifyContent="center" gap="45px" p="15px">
-        {testimonial.map((card) => (
-          <Flex
-            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-            w="470px"
+    return (
+        <Flex
+            h="740px"
+            w="100%"
+            gap="40px"
             flexDirection="column"
-            p="45px"
-            gap="5px"
-            borderRadius="20px"
-          >
-            <Text fontWeight="400" fontSize="18px" lineHeight="25px">
-              {card.note}
+            justifyContent="center"
+        >
+            <Text
+                as="h1"
+                fontFamily="Poppins"
+                fontSize="20px"
+                fontWeight="500"
+                lineHeight="30px"
+                color="#20B15A"
+                mt="3rem"
+                ml="3rem"
+            >
+                Testimonials
+            </Text>
+            <Text
+                as="h2"
+                fontFamily="Poppins"
+                fontSize="30px"
+                fontWeight="600"
+                lineHeight="30px"
+                mt="3rem"
+                ml="3rem"
+                maxW="500px"
+            >
+                See what our customer say about us
             </Text>
 
-            <Flex gap="10px" alignItems="center">
-              <Image src={card.image} maxW="60px" w="100%" h="60px" />
-              <Box>
-                <Text fontWeight="600" fontSize="20px" lineHeight="25px">
-                  {card.title}
-                </Text>
-                <Text fontWeight="400" fontSize="18px" lineHeight="25px">
-                  {card.occupation}
-                </Text>
-              </Box>
+            <Flex justifyContent="center" gap="45px" p="15px">
+                {testimonial.map((card) => (
+                    <Flex
+                        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                        w="470px"
+                        flexDirection="column"
+                        p="45px"
+                        gap="5px"
+                        borderRadius="20px"
+                    >
+                        <Text
+                            fontWeight="400"
+                            fontSize="18px"
+                            lineHeight="25px"
+                        >
+                            {card.note}
+                        </Text>
 
+                        <Flex gap="10px" alignItems="center">
+                            <Image
+                                src={card.image}
+                                maxW="60px"
+                                w="100%"
+                                h="60px"
+                            />
+                            <Box>
+                                <Text
+                                    fontWeight="600"
+                                    fontSize="20px"
+                                    lineHeight="25px"
+                                >
+                                    {card.title}
+                                </Text>
+                                <Text
+                                    fontWeight="400"
+                                    fontSize="18px"
+                                    lineHeight="25px"
+                                >
+                                    {card.occupation}
+                                </Text>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                ))}
             </Flex>
-          </Flex>
-        ))}
-      </Flex>
-    </Flex>
-  );
+        </Flex>
+    );
 };
