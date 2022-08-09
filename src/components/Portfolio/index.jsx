cd9da583d9315393ad40963dde61c32aa8662ca1
nestonzin <dinestoor@outlook.com>
2022-08-09 03:01:01 -0300

    fix and style portfolio

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -5,75 +5,104 @@ import Portfolio3 from "../../assets/portfolio_3.png";
 import Portfolio4 from "../../assets/portfolio_4.png";
 
 export const Portfolio = () => {
-    return (
-        <Box>
-            <Flex flexDirection="column">
-                <Text
-                    color="#20B15A"
-                    mr="45px"
-                    mt="3rem"
-                    ml="3rem"
-                    maxW="200px"
-                    fontWeight="600"
-                    fontSize="20px"
-                >
-                    OUR PORTFOLIO
-                </Text>
-                <Text
-                    mr="45px"
-                    mt="3rem"
-                    ml="3rem"
-                    maxW="450px"
-                    fontWeight="600"
-                    fontSize="30px"
-                >
-                    We provide the Perfect Solution to your business growth
-                </Text>
-                <Flex justify="center">
-                    <SimpleGrid columns={2} spacing={10}>
-                        <div>
-                            <Image src={Portfolio1} maxW="500px" />
-                            <Text>Digital Marketing Agency Website</Text>
-                            <Text maxW="300px">
-                                This is a website for a client who want to
-                                achieve their goals and meet their users needs
-                                while also increasing their reach. Accross all
-                                platforms. This is a website rebrand
-                            </Text>
-                        </div>
-                        <div>
-                            <Image src={Portfolio2} height="500px" />
-                            <Text>Digital Marketing Agency Website</Text>
-                            <Text>
-                                This is a website for a client who want to
-                                achieve their goals and meet their users needs
-                                while also increasing their reach. Accross all
-                                platforms. This is a website rebrand
-                            </Text>
-                        </div>
-                        <div>
-                            <Image src={Portfolio3} height="500px" />
-                            <Text>Digital Marketing Agency Website</Text>
-                            <Text>
-                                This is a website for a client who want to
-                                achieve their goals and meet their users needs
-                                while also increasing their reach. Accross all
-                                platforms. This is a website rebrand
-                            </Text>
-                        </div>
-                        <div>
-                            <Image src={Portfolio4} height="500px" />
-                            <Text>Digital Marketing Agency Website</Text>
-                            <Text>
-                                This is a website for a client who want to
-                                achieve their goals and meet their users needs
-                                while also increasing their reach. Accross all
-                                platforms. This is a website rebrand
-                            </Text>
-                        </div>
-                    </SimpleGrid>
-                </Flex>
+  return (
+    <Box>
+      <Flex flexDirection="column">
+        <Text
+          color="#20B15A"
+          mr="45px"
+          mt="3rem"
+          ml="3rem"
+          maxW="200px"
+          fontWeight="600"
+          fontSize="20px"
+        >
+          OUR PORTFOLIO
+        </Text>
+        <Text
+          mr="45px"
+          mt="3rem"
+          ml="3rem"
+          maxW="450px"
+          fontWeight="600"
+          fontSize="30px"
+        >
+          We provide the Perfect Solution to your business growth
+        </Text>
+        <Flex justify="center">
+          <SimpleGrid columns={2} spacing={10}>
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              gap="20px"
+              p="10px"
+            >
+              <Image src={Portfolio1} maxW="500px" w="100%" />
+              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
+                Digital Marketing Agency Website
+              </Text>
+              <Text
+                fontFamily="Poppins"
+                fontWeight="400"
+                lineHeight="21px"
+                fontSize="15px"
+              >
+                This is a website for a client who want to achieve their goals
+                and meet their users needs while also increasing their reach.
+                Accross all platforms. This is a website rebrand
+              </Text>
             </Flex>
-        </Box>
-    );
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              gap="20px"
+              p="10px"
+            >
+              <Image src={Portfolio2} maxW="500px" w="100%" />
+              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
+                Digital Marketing Agency Website
+              </Text>
+              <Text>
+                This is a website for a client who want to achieve their goals
+                and meet their users needs while also increasing their reach.
+                Accross all platforms. This is a website rebrand
+              </Text>
+            </Flex>
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              gap="20px"
+              p="10px"
+            >
+              <Image src={Portfolio3} maxW="500px" w="100%" />
+              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
+                Digital Marketing Agency Website
+              </Text>
+              <Text>
+                This is a website for a client who want to achieve their goals
+                and meet their users needs while also increasing their reach.
+                Accross all platforms. This is a website rebrand
+              </Text>
+            </Flex>
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              gap="20px"
+              p="10px"
+            >
+              <Image src={Portfolio4} maxW="500px" w="100%" />
+              <Text fontFamily="Poppins" fontWeight="600" lineHeight="34px">
+                Digital Marketing Agency Website
+              </Text>
+              <Text>
+                This is a website for a client who want to achieve their goals
+                and meet their users needs while also increasing their reach.
+                Accross all platforms. This is a website rebrand
+              </Text>
+            </Flex>
+          </SimpleGrid>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
