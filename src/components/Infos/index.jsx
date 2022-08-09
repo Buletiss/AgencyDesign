1019cc9fc562e8d984d03469cc68da4a43d5caf0
ZeroBulletiss <vinicius1997feitosa@gmail.com>
2022-08-09 19:46:41 -0300

    infor margin

--- src/components/Infos/index.jsx
+++ src/components/Infos/index.jsx
@@ -5,50 +5,51 @@ import rating from "../../assets/rating.png";
 import timeline from "../../assets/timeline.png";
 
 export const Infos = () => {
-  const cardInfo = [
-    {
-      image: plan,
-      description: "Completed Projects",
-      statistic: "100+",
-    },
-    {
-      image: raise,
-      description: "Customer Satisfaction",
-      statistic: "20%",
-    },
-    {
-      image: rating,
-      description: "Raised by Clients",
-      statistic: "$10M",
-    },
-    {
-      image: timeline,
-      description: "Years in Business",
-      statistic: "2 yrs",
-    },
-  ];
+    const cardInfo = [
+        {
+            image: plan,
+            description: "Completed Projects",
+            statistic: "100+",
+        },
+        {
+            image: raise,
+            description: "Customer Satisfaction",
+            statistic: "20%",
+        },
+        {
+            image: rating,
+            description: "Raised by Clients",
+            statistic: "$10M",
+        },
+        {
+            image: timeline,
+            description: "Years in Business",
+            statistic: "2 yrs",
+        },
+    ];
 
-  return (
-    <Flex
-      w="100%"
-      h="150px"
-      justifyContent="center"
-      color="#FAFFFB"
-      alignItems="center"
-    >
-      <Stack direction="row" spacing="85px">
-        {cardInfo.map((card) => (
-          <VStack>
-            <Image src={card.image} w="100%" maxW="70px" h="70px" />
-            <Text color="black" fontSize="16px">
-              {card.description}
-            </Text>
-            <Text color="green" fontWeight="bold" fontSize="40px">
-              {card.statistic}
-            </Text>
-          </VStack>
-        ))}
-      </Stack>
-    </Flex>
-  );
+    return (
+        <Flex
+            w="100%"
+            h="150px"
+            justifyContent="center"
+            color="#FAFFFB"
+            alignItems="center"
+            marginBottom="100px"
+        >
+            <Stack direction="row" spacing="85px">
+                {cardInfo.map((card) => (
+                    <VStack>
+                        <Image src={card.image} w="100%" maxW="70px" h="70px" />
+                        <Text color="black" fontSize="16px">
+                            {card.description}
+                        </Text>
+                        <Text color="green" fontWeight="bold" fontSize="40px">
+                            {card.statistic}
+                        </Text>
+                    </VStack>
+                ))}
+            </Stack>
+        </Flex>
+    );
 };
